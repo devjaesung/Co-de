3696181c16a51c6a32e5440fd401666ec1e8e1d5
Jaesung Ko <jaesungko.dev@gmail.com>
2022-05-23 16:11:26 +0900

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import {Container} from '@mui/material'
+// import Loginpage from './components/loginpage/Loginpage'
+// import Mainpage from './components/mainpage/Mainpage'
+// import Profilepage from './components/profilepage/Profilepage'
+// import Mypage from './components/mypage/Mypage'
+// import Writepage from './components/writepage/Writepage'
 
 function App() {
   return (
